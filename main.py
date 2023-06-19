9fc2d8894a3441f26858e88880c6aabb85f31227
Jaimboh <110313204+Jaimboh@users.noreply.github.com>
2023-06-19 21:30:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import streamlit as st
 from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
-from langchain.agents.agent import AgentAction, AgentFinish
 from langchain.prompts import StringPromptTemplate
+
 from langchain import LLMChain
-from pydantic import Field
 from langchain.chat_models import ChatOpenAI
-from langchain.tools import DuckDuckGoSearchRun
+from langchain.tools import DuckDuckGoSearchRun 
+
 from typing import List, Union
+from langchain.schema import AgentAction, AgentFinish
 import re
-import asyncio
-asyncio.set_event_loop(asyncio.new_event_loop())
+import langchain
 import os
 import chainlit as cl
 
-api_key = st.secrets["openai"]["api_key"]
-os.environ["OPENAI_API_KEY"] = api_key
+os.environ["OPENAI_API_KEY"] = "Your_api_key"
+
 template = """
 Answer the following questions as best you can, but speaking as a passionate construction expert. You have access to the following tools:
 
@@ -29,52 +28,77 @@ Action Input: Provide the input required for the chosen tool
 Observation: Describe the result obtained from the action
 ...(Repeat several times of the Thought/Action/Action Input/Observation as needed)
 Thought: Now I have the final answer!
-Final Answer: {Final AnswerTool}
-"""
+Final Answer: Provide your final answer from the perspective of an experienced construction professional
+
+Let's get started!
+Question: {input}
+{agent_scratchpad}"""
 
 class CustomPromptTemplate(StringPromptTemplate):
+    # The template to use
     template: str
+    # The list of tools available
     tools: List[Tool]
-    input_variables: List[str]
+    
     def format(self, **kwargs) -> str:
+        # Get the intermediate steps (AgentAction, Observation tuples)
+        # Format them in a particular way
         intermediate_steps = kwargs.pop("intermediate_steps")
         thoughts = ""
         for action, observation in intermediate_steps:
             thoughts += action.log
             thoughts += f"\nObservation: {observation}\nThought: "
+        # Set the agent_scratchpad variable to that value
         kwargs["agent_scratchpad"] = thoughts
+        # Create a tools variable from the list of tools provided
         kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+        # Create a list of tool names for the tools provided
         kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
         return self.template.format(**kwargs)
+
 
 
 class CustomOutputParser(AgentOutputParser):
     
     def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
         if "Final Answer:" in llm_output:
             return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
                 return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
                 log=llm_output,
             )
-        regex = r"Action\s*\d*\s*:(.?)\nAction\s\d*\s*Input\s*\d*\s*:[\s](.)"
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
         match = re.search(regex, llm_output, re.DOTALL)
         if not match:
             raise ValueError(f"Could not parse LLM output: `{llm_output}`")
         action = match.group(1).strip()
         action_input = match.group(2)
+        # Return the action and action input
         return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
 
+
 def search_online(input_text):
+    search = DuckDuckGoSearchRun().run(f"site:sweets.construction.com {input_text}")
+    return search
+
+def search_supplier(input_text):
+    search = DuckDuckGoSearchRun().run(f"site:thomasnet.com {input_text}")
+    return search
+
+def search_material(input_text):
     search = DuckDuckGoSearchRun().run(f"site:Americanbuildsupply.com {input_text}")
     return search
 
 def search_equipment(input_text):
     search = DuckDuckGoSearchRun().run(f"site:machinerytrader.com {input_text}")
     return search
-
+  
 def search_general(input_text):
     search = DuckDuckGoSearchRun().run(f"{input_text}")
-    return search
+    return search 
 
 @cl.langchain_factory(use_async=False)
 def agent():
@@ -90,6 +114,16 @@ def agent():
             description="useful for when you need to search for construction-related information online"
         ),
         Tool(
+            name = "Search supplier",
+            func=search_supplier,
+            description="useful for when you need to search for construction suppliers"
+        ),
+        Tool(
+            name = "Search material",
+            func=search_material,
+            description="useful for when you need to search for construction materials"
+        ),
+        Tool(
             name = "Search equipment",
             func=search_equipment,
             description="useful for when you need to search for construction equipment"
@@ -98,6 +132,8 @@ def agent():
     prompt = CustomPromptTemplate(
         template=template,
         tools=tools,
+        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+        # This includes the `intermediate_steps` variable because that is needed
         input_variables=["input", "intermediate_steps"]
     )
 
@@ -106,25 +142,13 @@ def agent():
     llm_chain = LLMChain(llm=llm, prompt=prompt)
     tool_names = [tool.name for tool in tools]
 
+
     agent = LLMSingleActionAgent(llm_chain=llm_chain, output_parser=output_parser, stop=["\nObservation:"], allowed_tools=tool_names)
-    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent,
+                                                    tools=tools,
+                                                    verbose=True)
     return agent_executor
 
-# Create an instance of the agent_executor
+
+# Creating an instance of the agent_executor
 agent_executor = agent()
-
-# Streamlit app
-def main():
-    st.title("Construction Consultancy Bot👷‍♂️👷‍♀️🚧")
-    st.write("Ask construction-related questions and get expert advice!")
-
-    question = st.text_input("Enter your question:")
-    if st.button("Ask"):
-        if question:
-            response = agent_executor(question)
-            st.write(response["output"])
-        else:
-            st.write("Please enter a question.")
-
-if __name__ == "__main__":
-    main()
